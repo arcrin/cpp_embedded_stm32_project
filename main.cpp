a42b1ad67394ca839a07855d5915f48bed32f911
wbai <wbai@deltacontrols.com>
2024-06-06 19:54:59 -0700

    new environment added; investigating macro expansion

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <stdio.h>
+
+// #define EXPAND(x) x
+// #define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+// #define PRINT_INT(x) printf("%d\n", x);
+
+#define OL 123          // Object-like macro
+#define FL(x) ((x)+1)   // function-like macro
+
+enum EPOLL_EVENTS {
+    EPOLLIN = 0x001,
+    #define EPOLLIN EPOLLIN
+}
+
+
 
 int main(int, char**){
-    std::cout << "Hello, from cpp_embedded_stm32_project!\n";
+    // std::cout << "Hello, from cpp_embedded_stm32_project!\n";
+    int result = ADD(1,2);
+    printf("result: %d\n", result);
 }
