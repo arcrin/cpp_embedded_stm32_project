c21fa7a28a70f5b667d6d89f64e59852f9cc660b
wbai <wbai@deltacontrols.com>
2024-06-10 18:34:25 -0700

    macro

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdint.h>
 
 // #define EXPAND(x) x
 // #define FOR_EACH_HELPER(macro, a1, ...) macro(a1) __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
@@ -13,9 +14,27 @@
 //     #define EPOLLIN EPOLLIN;
 // }
 
+#define LPAREN (
+#define ID(arg) arg
+#define ID2(arg) arg
+#define ADD(a, b) a + b
+
+#define TO_STRING(x) #x
+#define CONCATENATE(x, y) x ## y
+#define MAKE_STRING_AND_CONCAT(x, y) TO_STRING(x ## y)
+
 
 
 int main(int, char**){
-    // std::cout << "Hello, from cpp_embedded_stm32_project!\n";
+    char cstr1cstr2[] = "Hello World!"; 
+        
+    uint8_t var1 = 42;
+    uint8_t var2 = 100;
+    printf("%s\n", TO_STRING(Hello));
+    printf("%d\n", CONCATENATE(var, 1));
+    printf("%d\n", CONCATENATE(var, 2));
+    printf("%s\n", MAKE_STRING_AND_CONCAT(Hello, World!));
+    printf("%d\n", ID()ID(5));
+    
     return 0;
 }
